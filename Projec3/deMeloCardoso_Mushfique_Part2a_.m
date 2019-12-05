c46a5fb40fd275a6d0dac868393b1456f335d31c
João Paulo <card_joao@users.noreply.github.com>
2019-12-05 00:18:42 -0600

    Update code

--- Projec3/deMeloCardoso_Mushfique_Part2a_.m
+++ Projec3/deMeloCardoso_Mushfique_Part2a_.m
@@ -2,8 +2,27 @@ clear;
 clc;
 close all;
 
-[test_signalOriginal, FsO]=audioread('OriginalTestSignal48.wav');
-[test_signal, Fs]=audioread('testSignal48.wav');
+[test_signalOriginal, FsO]=audioread('OriginalTestSignal8.wav');
+[test_signal, Fs]=audioread('testSignal8.wav');
+
+%% Cross correlation
+[xc,lags] = xcorr(test_signal,test_signalOriginal);
+[~,I] = max(abs(xc));
+figure
+stem(lags,xc,'filled')
+legend(sprintf('Maximum at lag %d',lags(I)))
+title('Sample Cross-Correlation Sequence');
+
+test_signal = delayseq(test_signal,383);
+
+
+%% Cross correlation
+[xc,lags] = xcorr(test_signal,test_signalOriginal);
+[~,I] = max(abs(xc));
+figure
+stem(lags,xc,'filled')
+legend(sprintf('Maximum at lag %d',lags(I)))
+title('Sample Cross-Correlation Sequence');
 
 if(0)
     for order=10:10:100
@@ -19,22 +38,30 @@ if(0)
     end
 end
 
-if(0)
+clear lags;
+clear xc;
+clear I;
+clear Fs;
+clear FsO;
+
+if(1)
     
-    for na=5:1:10
+%     for na=5:1:10
         %nb zeros, and exactly na poles.
-        [b,a] = stmcb(test_signal,test_signalOriginal,0,na);
+        [b,a] = stmcb(test_signal,test_signalOriginal,0,2);
         figure;
         [H1,w1] = freqz(b,a);
         freqz(b,a);
         title("Frequency response STMCB method - denominator order = " + num2str(na));
-    end
+%     end
 
 end
 
+return;
+
 if(1)
-    order = 100;
-    [d1,p1] = aryule(test_signal,order);
+    order = 200;
+    [d1,p1] = aryule(shifted_test_signal,order);
     figure;
     freqz(sqrt(p1),d1);
     title("Frequency response aryule method - denominator order = " + num2str(order));
@@ -46,14 +73,14 @@ if(1)
 
 end
 
-[originalMusic, FsOriginalMusic]=audioread('music4816.wav');
+[originalMusic, FsOriginalMusic]=audioread('human4816.wav');
 
 filteredOriginalMusic = filter(sqrt(p1),d1,originalMusic);
 
 % sound(filteredOriginalMusic,FsOriginalMusic);
 % audiowrite('filteredOriginalMusic48.wav', filteredOriginalMusic,48e3);
 
-[MusicWith, FsMusicWith]=audioread('music4816Without.wav');
+[MusicWith, FsMusicWith]=audioread('human4816Without.wav');
 
 %% Cross correlation
 [xc,lags] = xcorr(MusicWith,originalMusic);
@@ -64,7 +91,7 @@ legend(sprintf('Maximum at lag %d',lags(I)))
 title('Sample Cross-Correlation Sequence');
 
 %%
-shifted_data = delayseq(originalMusic,-304);
+shifted_data = delayseq(originalMusic,1430);
 
 %% Cross correlation
 [xc,lags] = xcorr(MusicWith,shifted_data);
@@ -75,7 +102,35 @@ legend(sprintf('Maximum at lag %d',lags(I)))
 title('Sample Cross-Correlation Sequence');
 
 %%
-outputNoiseCancel = filteredOriginalMusic-MusicWith;
+fftSignal = fft(originalMusic,48e3);
+fftSignal = fftshift(fftSignal);
+figure;
+plot(abs(fftSignal));
+title('Original Signal spectrum');
+ylabel('Amplitude');
+
+fftSignal = fft(MusicWith,48e3);
+fftSignal = fftshift(fftSignal);
+figure;
+plot(abs(fftSignal));
+title('Recorded Signal spectrum');
+ylabel('Amplitude');
+
+fftSignal = fft(filteredOriginalMusic,48e3);
+fftSignal = fftshift(fftSignal);
+figure;
+plot(abs(fftSignal));
+title('Filtered Signal spectrum');
+ylabel('Amplitude');
+
+outputNoiseCancel = MusicWith-filteredOriginalMusic;
+
+fftSignal = fft(outputNoiseCancel,48e3);
+fftSignal = fftshift(fftSignal);
+figure;
+plot(abs(fftSignal));
+title('Output Signal spectrum');
+ylabel('Amplitude');
 
 sound(outputNoiseCancel,48e3);
 
